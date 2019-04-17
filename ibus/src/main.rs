bd4a168f0403e45b85a7248073701941c4e434b3
殷啟聰 | Kai-Chung Yan <seamlik@debian.org>
2019-04-17 21:38:14 +0200

    DO NOT MERGE

--- ibus/src/main.rs
+++ ibus/src/main.rs
@@ -1,5 +1,34 @@
+use failure::Error;
+use failure::ResultExt;
+use std::process::Command;
+
 pub mod dbus;
 
 fn main() {
-    println!("Hello, world!");
+    println!("{}", get_dbus_address_ibus().unwrap());
+}
+
+/// Gets the D-Bus address for iBus.
+///
+/// This is required for talking to iBus as it does not operate through the central D-Bus.
+///
+/// # Errors
+///
+/// This function queries the address by running `ibus address`, so expect errors related to process executions.
+pub fn get_dbus_address_ibus() -> Result<String, Error> {
+    let output = Command::new("ibus")
+        .arg("address")
+        .output()
+        .context("Failed to execute `ibus address`!")?;
+
+    if output.status.success() {
+        Ok(String::from_utf8(output.stdout)
+            .context("`ibus` returned invalid UTF-8 data!")?
+            .trim()
+            .to_string())
+    } else {
+        Err(failure::err_msg(
+            "Could not find the D-Bus address of iBus! Perhaps it's not running?",
+        ))
+    }
 }
